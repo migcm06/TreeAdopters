122c9c93b5416c2f68acc128c43c3231b346882a
Miguel Cuello <luismiguelcm2015@gmail.com>
2023-08-07 15:44:16 -0500

    Solving Stylelint and ESLint errors

--- main.js
+++ main.js
@@ -1,37 +1,39 @@
-const speaker = [
+const speakers = [
   {
     photo: './images/speaker1.png',
     name: 'Yochai Benkler',
     ocupation: 'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
-    description: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
+    description: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
   },
   {
     photo: './images/speaker2.png',
     name: 'Kilnam Chon',
-    description: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006'
+    description: 'Benkler studies commons-based peer production, and published his seminal book, The Wealth of Networks in 2006',
   },
   {
     photo: './images/speaker3.png',
     name: 'SohYeong Noh',
     ocupation: 'Director of Art Centre Nabi and a board member of CC Korea',
-    description: 'Kilnam Chon helped bring the internet to Asia and is an outspoken advocate for the open web and digital com-mons. In 2012. he was inducted into the inaugural class of the Internet Society’s (ISOC) Internet Hall of Fame'
+    description: 'Kilnam Chon helped bring the internet to Asia and is an outspoken advocate for the open web and digital com-mons. In 2012, he was inducted into the inaugural class of the Internet Society’s (ISOC) Internet Hall of Fame',
   },
   {
     photo: './images/speaker4.png',
     name: 'Julia Leda',
     ocupation: 'President of Young Pirates of Europe',
-    description: 'European ingetration, political democracy and participation of youth through online as her major condern, Reda’s report outlining potential changes to EU copyright law was approved by the Parliament in July'
+    description: 'European integration, political democracy and participation of youth through online as her major concern, Reda’s report outlining potential changes to EU copyright law was approved by the Parliament in July',
   },
   {
     photo: './images/speaker5.png',
-    name: 'Lila tretikov',
+    name: 'Lila Tretikov',
     ocupation: 'Executive Director of the Wikimedia Foundation',
-    description: 'Lila Tretikov is the Executive of the Wikimedia Foundation, the nonprofit organization that operates Wikipedia. Wikipedia is freely available in 290 languag-es and used by nearly half a billion people around the world every month.'
+    description: 'Lila Tretikov is the Executive of the Wikimedia Foundation, the nonprofit organization that operates Wikipedia. Wikipedia is freely available in 290 languages and used by nearly half a billion people around the world every month.',
   },
   {
     photo: './images/speaker6.png',
     name: 'Ryan Merkley',
-    ocupation: 'CEO of Creativve Commons, ex COO of the Mozilla Foundation',
-    description: 'Ryan had been leading open-source projects at the Mozilla Foundation such as the open-source move-ment'
-  }
-]
+    ocupation: 'CEO of Creative Commons, ex COO of the Mozilla Foundation',
+    description: 'Ryan had been leading open-source projects at the Mozilla Foundation such as the open-source movement',
+  },
+];
+
+console.log(speakers);
